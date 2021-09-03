4a2423cc87bb7721d663fb8ea24707004c301f53
hnrkndrssn <henrik@henrikandersson.org>
2021-09-03 13:13:23 +1000

    Add a way to override the default test filter when running dotnet test

--- source/NukeBuildComponents/ITest.cs
+++ source/NukeBuildComponents/ITest.cs
@@ -1,11 +1,15 @@
 using Nuke.Common;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.ValueInjection;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 namespace Octopus.NukeBuildComponents
 {
     public interface ITest : IOctopusNukeBuild
     {
+        [Parameter("Filter expression to run selective tests. Default is 'FullyQualifiedName!~Integration.Tests'")] 
+        string TestFilter => ValueInjectionUtility.TryGetValue(() => TestFilter) ?? "FullyQualifiedName!~Integration.Tests";
+
         Target Test => _ => _
             .TryDependsOn<ICompile>(x => x.Compile)
             .Executes(() =>
@@ -15,7 +19,7 @@ namespace Octopus.NukeBuildComponents
                     .SetConfiguration(Config)
                     .SetNoBuild(true)
                     .EnableNoRestore()
-                    .SetFilter(@"FullyQualifiedName\!~Integration.Tests"));
+                    .SetFilter(TestFilter));
             });
     }
 }
