c7edc3c6fe9346d576ade181800deff763d82116
Cail Young <cail.young@octopus.com>
2022-04-07 13:33:12 +1000

    Make octoversion work with interfaces with default implementations

--- source/NukeBuildComponents/IOctopusNukeBuild.cs
+++ source/NukeBuildComponents/IOctopusNukeBuild.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.OctoVersion;
+using Nuke.Common.Utilities;
+using Nuke.Common.ValueInjection;
 
 namespace Octopus.NukeBuildComponents
 {
@@ -14,11 +20,41 @@ namespace Octopus.NukeBuildComponents
 
         [Solution] Solution? Solution => TryGetValue(() => Solution);
 
-        [OctoVersion] OctoVersionInfo? OctoVersionInfo => TryGetValue(() => OctoVersionInfo);
+        [Parameter("Branch name for OctoVersion to use to calculate the version number."
+            , Name = "OCTOVERSION_CurrentBranch")]
+        string BranchName => TryGetValue(() => BranchName);
+
+        [Parameter("Whether to auto-detect the branch name - this is okay for a local " +
+                   "build, but should not be used under CI.")]
+        bool AutoDetectBranch => IsLocalBuild;
+
+        [Required]
+        [OctoVersionThatWorksWithBuildComponentsAttribute(AutoDetectBranchParameter = nameof(AutoDetectBranch),
+            BranchParameter = nameof(BranchName),
+            UpdateBuildNumber = true,
+            Framework = "net6.0")]
+        OctoVersionInfo OctoVersionInfo => TryGetInjectedOctoVersionInfo(() => OctoVersionInfo);
 
         AbsolutePath SourceDirectory => RootDirectory / "source";
         public AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
         public AbsolutePath PublishDirectory => RootDirectory / "publish";
         public AbsolutePath LocalPackagesDir => RootDirectory / ".." / "LocalPackages";
+
+        private static readonly Dictionary<MemberInfo, object> s_valueCache = new Dictionary<MemberInfo, object>();
+
+        // we need this custom method instead of the "normal" TryGetValue,
+        // as we need to pass `instance: this` down
+        OctoVersionInfo TryGetInjectedOctoVersionInfo(Expression<Func<OctoVersionInfo>> parameterExpression)
+        {
+            var parameter = parameterExpression.GetMemberInfo();
+
+            OctoVersionInfo GetValue()
+            {
+                var attribute = parameter.GetCustomAttribute<OctoVersionThatWorksWithBuildComponentsAttribute>().NotNull();
+                return (OctoVersionInfo) attribute!.TryGetValue(parameter, instance: this)!;
+            }
+
+            return (OctoVersionInfo) (s_valueCache[parameter] = s_valueCache.GetValueOrDefault(parameter) ?? GetValue());
+        }
     }
 }
